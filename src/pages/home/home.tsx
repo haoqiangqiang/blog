ea64e3ed272f6ee706c13950385dc5205ee09928
haoqiangqiang <826173335@qq.com>
2020-04-04 17:39:00 +0800

    通过axios获取数据

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { connect } from 'react-redux';
 import { 
     HomeWrapper,
     HomeLeft,
@@ -8,9 +9,10 @@ import List from './components/List';
 import Recommend from './components/Recommend';
 import Topic from './components/Topic';
 import Writer from './components/Writer';
+import axios from 'axios';
 
 export interface Props {
-    
+    changeHomeData: any,
 }
  
 export interface State {
@@ -32,8 +34,24 @@ class Home extends React.Component<Props, State> {
             </HomeWrapper>
          );
     }
+    componentDidMount() {
+        axios.get('/api/home.json').then(res => {
+            const result = res.data.data;
+            const action = {
+                type: 'change_home_data',
+                topicList: result.topicList,
+                articleList: result.articleList,
+                recommendList: result.recommendList,
+            }
+            this.props.changeHomeData(action);
+        }); 
+    }
 }
- 
-export default Home;
+ const mapDispatch = (dispatch: any) => ({
+    changeHomeData(action: any) {
+        dispatch(action)
+    }
+ })
+export default connect(null, mapDispatch)(Home);
 
 // "<scene name="scene_55487bcb-7496-404a-972a-aca009766198" title="u=679906364,2223690437&amp;fm=26&amp;gp=0.jpg" onstart="" havevrimage="true" thumburl="https://sheencity-bj.oss-cn-hangzhou.aliyuncs.com/venus/dev/resources/95dbd38a/d943740d-65ed-4086-a6c6-642adee9b91a/panos/55487bcb-7496-404a-972a-aca009766198/thumb.jpg" lat="" lng="" heading="" visible="true" action="snowballs()" actionname="下雪" weatherflakes="3500" weatherspeed="5" weatherdegreename="大雪" xmlns="http://www.w3.org/1999/xhtml" bgmusic="C:\Users\86155\AppData\Roaming\Venus/krpano-project/krpano-project/d943740d-65ed-4086-a6c6-642adee9b91a/musicTemp/downlifiting.mp3" musicname="downlifiting.mp3"><view hlookat="0" vlookat="0" fovtype="MFOV" fov="120" fovmin="70" fovmax="140"></view><preview url="https://sheencity-bj.oss-cn-hangzhou.aliyuncs.com/venus/dev/resources/95dbd38a/d943740d-65ed-4086-a6c6-642adee9b91a/panos/55487bcb-7496-404a-972a-aca009766198/preview.jpg"></preview><image type="CUBE" multires="true" tilesize="512" if="!webvr.isenabled"><level tiledimagewidth="1024" tiledimageheight="1024"><cube url="https://sheencity-bj.oss-cn-hangzhou.aliyuncs.com/venus/dev/resources/95dbd38a/d943740d-65ed-4086-a6c6-642adee9b91a/panos/55487bcb-7496-404a-972a-aca009766198/mres_%s/l1/%v/l1_%s_%v_%h.jpg"></cube></level><level tiledimagewidth="2560" tiledimageheight="2560"><cube url="https://sheencity-bj.oss-cn-hangzhou.aliyuncs.com/venus/dev/resources/95dbd38a/d943740d-65ed-4086-a6c6-642adee9b91a/panos/55487bcb-7496-404a-972a-aca009766198/mres_%s/l2/%v/l2_%s_%v_%h.jpg"></cube></level></image><image if="webvr.isenabled"><cube url="https://sheencity-bj.oss-cn-hangzhou.aliyuncs.com/venus/dev/resources/95dbd38a/d943740d-65ed-4086-a6c6-642adee9b91a/panos/55487bcb-7496-404a-972a-aca009766198/mobile_%s.jpg"></cube></image></scene>"
